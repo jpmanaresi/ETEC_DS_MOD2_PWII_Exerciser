973232e138730ae1c91ff82f9b65ccdc36fb2b60
jpmanaresi <jpmanaresi@gmail.com>
2022-10-18 20:03:01 -0300

    atv Criada

---
+++ src/Web/Motor.js
@@ -0,0 +1,31 @@
+class Motor {
+     acelerar() {
+        return "";
+    }
+}
+
+class MotorFusca extends Motor {
+
+    acelerar() {
+        return "Vruumm...";
+    }
+};
+
+class MotorCelta extends Motor {
+    acelerar () {
+        return "Vruuuuuuuuuummmm..."
+    }
+}; 
+
+class MotorFerrari extends Motor {
+    acelerar () {
+        return "VRRRRRRRUUUUUUUUUUUUUUMMMMMMMM......."
+    }
+}; 
+
+class MotorAudi extends Motor {
+
+    acelerar() {
+        return 'VVVVVVVRRRRRRRRRRRRRUUUUUUUUUUUUUUUUUUUUUUUMMMMMMMMMMMMMMMMMMMMMMMM!!!!!!!!!!!!!'
+    }
+}
